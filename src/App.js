bd7f10712c7d878b5fd989f40c86c1844fa8dfd2
edd-ie <ogendoeddie@gmail.com>
2023-05-01 13:49:27 +0300

    Site Navigation and route redirects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './components/Home'
+import Nav from './components/Nav'
 import Chat from './components/Chat'
 
 function App() {
   return (
-    <div className="App">
-      <Chat/>
-    </div>
+    <BrowserRouter>
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/chat" element={<Chat/>}/>
+            </Routes>
+        </BrowserRouter>
   );
 }
 
