79ad5610c6a51c62b1106ca01d5e684529e580db
edd-ie <ogendoeddie@gmail.com>
2023-05-01 14:49:38 +0300

    Navigation error fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/Home'
-import Nav from './components/Nav'
 import Chat from './components/Chat'
 
 function App() {
   return (
     <BrowserRouter>
-            <Nav/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/chat" element={<Chat/>}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import './Home.css'
+import { Link } from "react-router-dom";
 
 export default function Home(){
     return(
-        <div>HOME</div>
+        <div>
+            <div id="nav">
+                <Link to="/">Home</Link>
+                <Link to="/chat">Chat</Link>
+            </div>
+            HOME
+        </div>
     )
 }
