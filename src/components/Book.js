d2af0239ca19e54a134720c6fca60ea1eb814c1a
dantezorenge <orenged524@gmail.com>
2023-05-04 11:19:28 +0300

    new update

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,51 +1,95 @@
+// import './book.css';
+// import React from 'react';
+
+// export default function Flipbook() {
+
+  
+
+
+//     function handleSubmit(e){
+//         e.preventDefault();
+//         let form = e.target;
+//         fetch("https://api-inference.huggingface.co/models/facebook/bart-large-cnn",{
+//         method: "POST",
+//         headers: { Authorization: `Bearer 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` },
+//         body: JSON.stringify({inputs:`${query}`}),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log("file: ai.js:82 -> form.addEventListener -> data:", data);
+        
+//     })
+//     .catch(err => console.log(err))
+
+//     }   
+//     return(
+//    <div id='host'> 
+//     <div className="book-container">
+//     <input id="checkbox-cover" type="checkbox"/>
+//     <div className="book">
+//      <label for="checkbox-cover" class="cover"></label>
+//      <div className="backcover"></div>
+//      <div className="page page1">
+//         <div id='bookForm'> 
+//             <form onSubmit={handleSubmit}>
+//                 <input id="doc" type="text" name="name" placeholder='Fulltext'/>
+//                 <input type="submit" value="Summarize" id="summarySub"/>
+                
+//             </form>
+//         </div>
+//         <div id='summary'>
+//           <p></p>
+//         </div>
+//      </div>
+//   </div>
+// </div>
+//    </div>
+
+//   );
+// }
 import './book.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Flipbook() {
+  const [summary, setSummary] = useState('');
 
-  // fetch("https://api-inference.huggingface.co/models/facebook/bart-large-cnn",{
-  //       method: "POST",
-  //       headers: { Authorization: `Bearer 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` },
-  //       body: JSON.stringify({inputs:`${query}`}),
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {
-  //       console.log("file: ai.js:82 -> form.addEventListener -> data:", data);
-        
-  //   })
-  //   .catch(err => console.log(err))
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const query = form.elements.doc.value;
+    fetch("https://api-inference.huggingface.co/models/facebook/bart-large-cnn", {
+      method: "POST",
+      headers: { Authorization: `Bearer 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` },
+      body: JSON.stringify({ inputs: query }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Summary:", data[0].summary_text);
+        setSummary(data[0].summary_text);
+      })
+      .catch(err => console.log(err));
+  }
 
   return (
-    // <div>
-    //   <input type="checkbox" id="checkbox-cover" />
-    //   <div className="book">
-    //     <div className="cover">cover</div>
-    //         <label for="checkbox-cover"></label>
-    //     <div className="page">page</div>
-    //     <div className="back-cover">page</div>
-    //   </div>
-    // </div>
-
-   <div id='host'> 
-    <div className="book-container">
-    <input id="checkbox-cover" type="checkbox"/>
-    <div className="book">
-     <label for="checkbox-cover" class="cover"></label>
-     <div className="backcover"></div>
-     <div className="page page1">
-        <div id='bookForm'> 
-            <form >
-                <input id="doc" type="text" name="name" placeholder='Fulltext'/>
-                <input type="submit" value="Summarize" id="summarySub"/>
-            </form>
+    <div id='host'>
+      <div className="book-container">
+        <input id="checkbox-cover" type="checkbox" />
+        <div className="book">
+          <label htmlFor="checkbox-cover" className="cover"></label>
+          <div className="backcover"></div>
+          <div className="page page1">
+            <div id='bookForm'>
+              <form onSubmit={handleSubmit}>
+                <input id="doc" type="text" name="doc" placeholder='Fulltext' />
+                <input type="submit" value="Summarize" id="summarySub" />
+              </form>
+            </div>
+            <div id='summary'>
+              <p>{summary}</p>
+            </div>
+          </div>
         </div>
-        <div id='summary'>
-          <p></p>
-        </div>
-     </div>
-  </div>
-</div>
-   </div>
-
+      </div>
+    </div>
   );
 }
